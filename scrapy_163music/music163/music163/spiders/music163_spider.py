cb4c99c97a61bd42b259abf6576824eb271179cb
congeebrother <daniel_001@126.com>
2015-04-21 14:48:31 +0800

    scrapy init

---
+++ scrapy_163music/music163/music163/spiders/music163_spider.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class Music163SpiderSpider(scrapy.Spider):
+    name = "music163_spider"
+    allowed_domains = ["music.163.com"]
+    start_urls = (
+        'http://www.music.163.com/',
+    )
+
+    def parse(self, response):
+        pass
